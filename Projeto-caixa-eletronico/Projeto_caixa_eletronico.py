22b33cd68456ade729295f54322309e29201c339
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2023-01-28 07:12:24 -0300

    Create Projeto_caixa_eletronico.py
    Primeiros passos para criação do projeto de caixa eletronico

---
+++ Projeto-caixa-eletronico/Projeto_caixa_eletronico.py
@@ -0,0 +1,4 @@
+"""
+Com base no exercicio 071 do mundo 2 de Pyhton 3, vou tentar fazer um projeto de caixa eletronico
+em que ele vai sacar dinheiro, depositar dinheiro e visualizar o extrato bancário.
+"""
