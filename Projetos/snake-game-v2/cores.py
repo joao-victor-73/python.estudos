91ceb0d4ce3013329165ccb453dc3df43bb3cb7e
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2023-03-25 13:09:45 -0300

    snake-game-v2
    Uma segunda maneira de criar o jogo da cobrinha, sendo uma versão mais básica aprendida em uma vídeo aula!

---
+++ Projetos/snake-game-v2/cores.py
@@ -0,0 +1,7 @@
+def cor():
+    vermelho = (255, 0, 0)
+    verde = (0, 255, 0)
+    azul = (0, 0, 255)
+
+    # depois faço a função para colocar as cores
+    # Se for colocar cores, tem que ser em TUPLAS!!!    
