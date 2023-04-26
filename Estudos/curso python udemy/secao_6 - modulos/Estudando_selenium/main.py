067f27cad6dcfec1ebf1782aba9c188792d6dfe7
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2023-04-26 17:02:22 -0300

    Estudo sobre Selenium 1
    Um pequeno estudo / introdução ao que é o Selenium e o como usar.

---
+++ Estudos/curso python udemy/secao_6 - modulos/Estudando_selenium/main.py
@@ -0,0 +1,90 @@
+# < Começando um pequeno estudo sobre Selenium >
+# < (Aulas 315, 316, 317, 318, 319 ) >
+
+''' Aula 315:
+Selenium é uma biblioteca com diversos métodos que ajudam na automação web.
+Em suma, as funções permitem controlar o funcionamento de uma página e a 
+interação com ela de forma automática;
+
+Para termos essa automação, precisamos instalar um drive para o navegador, e com
+isso podermos utilizar o selenium sem problemas;
+
+Para cada navegador haverá um driver diferente. Para as aulas, será utilizado o
+Chrome.
+'''
+# Aula 316:
+# pip install selenium (com venv ativado)
+# Selenium - Automatizando tarefas no navegador
+
+# Aula 319: Código completo>
+
+# type: ignore
+# Selenium - Automatizando tarefas no navegador
+from pathlib import Path
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+# Chrome Options
+# https://peter.sh/experiments/chromium-command-line-switches/
+# Doc Selenium
+# https://selenium-python.readthedocs.io/locating-elements.html
+
+
+# Caminho para a raiz do projeto
+ROOT_FOLDER = Path(__file__).parent
+# Caminho para a pasta onde o chromedriver está
+CHROME_DRIVER_PATH = ROOT_FOLDER / 'drivers' / 'chromedriver'
+
+
+def make_chrome_browser(*options: str) -> webdriver.Chrome:
+    chrome_options = webdriver.ChromeOptions()
+
+    # chrome_options.add_argument('--headless')
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    chrome_service = Service(
+        executable_path=str(CHROME_DRIVER_PATH),
+    )
+
+    browser = webdriver.Chrome(
+        service=chrome_service,
+        options=chrome_options
+    )
+
+    return browser
+
+
+if __name__ == '__main__':
+    TIME_TO_WAIT = 10
+
+    # Example
+    # options = '--headless', '--disable-gpu',
+    options = ()
+    browser = make_chrome_browser(*options)
+
+    # Como antes
+    browser.get('https://www.google.com')
+
+    # Espere para encontrar o input
+    search_input = WebDriverWait(browser, TIME_TO_WAIT).until(
+        EC.presence_of_element_located(
+            (By.NAME, 'q')
+        )
+    )
+    search_input.send_keys('Hello World!')
+    search_input.send_keys(Keys.ENTER)
+
+    results = browser.find_element(By.ID, 'search')
+    links = results.find_elements(By.TAG_NAME, 'a')
+    links[0].click()
+
+    # Dorme por 10 segundos
+    sleep(TIME_TO_WAIT)
