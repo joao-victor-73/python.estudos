9e11078398a36b84633ccdca16244e8f4ca9e99d
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2023-05-13 12:27:17 -0300

    estudando curso DSA(11) - Projeto 01
    Começando os estudos para o projeto (versão 2 - jogável)
    
    e adicionado arquivos do capitulo

--- Estudos/CursoDSA - Analise de dados e Data Science/Projeto1/main.py
+++ Estudos/CursoDSA - Analise de dados e Data Science/Projeto1/main.py
@@ -1,4 +1,4 @@
-# Projeto 1 - Desenvolvimento de Game em Python - Aula 4/5
+# Projeto 1 - Desenvolvimento de Game em Python - Aula 5/5
 
 # Importando bibliotecas
 import random
@@ -17,6 +17,84 @@ def limpa_tela():
         _ = system('clear')
 
 
+def display_hangman(chances):
+    '''Função qie desemja a forca na tela.'''
+
+    # Lista de estários da forca
+    stages = [  # Estágio 6 (final)
+        '''
+        ----------
+        |        |
+        |        0
+        |       \\|/
+        |        |
+        |       / \\
+        -
+        ''',
+        # Estágio 5
+        '''
+        ----------
+        |        |
+        |        0
+        |       \\|/
+        |        |
+        |       / 
+        -
+        ''',
+        # Estágio 4
+        '''
+        ----------
+        |        |
+        |        0
+        |       \\|/
+        |        |
+        |       
+        -
+        ''',
+        # Estágio 3
+        '''
+        ----------
+        |        |
+        |        0
+        |       \\|
+        |        |
+        |       
+        -
+        ''',
+        # Estágio 2
+        '''
+        ----------
+        |        |
+        |        0
+        |        |
+        |        |
+        |       
+        -
+        ''',
+        # Estágio 1
+        '''
+        ----------
+        |        |
+        |        0
+        |        
+        |        
+        |       
+        -
+        ''',
+        # Estágio 0
+        '''
+        ----------
+        |        |
+        |        
+        |       
+        |        
+        |       
+        -
+        '''
+    ]
+    return stages[chances]
+
+
 def game():
 
     limpa_tela()
@@ -30,45 +108,64 @@ def game():
     # Escolhe randomicamente uma palavra:
     palavra = random.choice(palavras)
 
+    '''
     # List comprehension
     letras_descobertas = ['_' for letra in palavra]
+    '''
+
+    # Lista de letras da palavra
+    lista_letras_palavras = [letra for letra in palavra]
+
+    # Cria o tabuleiro com o caracter "_" multiplicado pelo comprimento da palavra
+    tabuleiro = ["_"] * len(palavra)
 
     # Número de chances
     chances = 6
 
-    # Lista para as letras erradas
-    letras_erradas = []
+    # Lista para as digitadas
+    letras_tentativas = []
 
     # Loop enquanto número de chances for maior do que zero:
     while chances > 0:
 
-        print(" ".join(letras_descobertas))
-        print(f"\nChances restantes: {chances}")
-        print("Letras erradas:", " ".join(letras_erradas))
+        print(display_hangman(chances))
+        print(f"Palavra: {tabuleiro}")
+        print("\n")
 
         # Tentativa
-        tentativa = input("\nDigite uma letra: ").lower()
+        tentativa = input("\nDigite uma letra: ").upper()
 
         # Condicional
-        if tentativa in palavra:
-            index = 0
+        if tentativa in letras_tentativas:
+            print("Você já tentou essa letra. Escolha outra!")
+            continue
 
-            for letra in palavra:
-                if tentativa == letra:
-                    letras_descobertas[index] = letra
-                index += 1
-        else:
-            chances -= 1
-            letras_erradas.append(tentativa)
+        letras_tentativas.append(tentativa)
 
         # Condicional 2
-        if "_" not in letras_descobertas:
-            print(f"\n Você venceu, a palavra era: {palavra}")
-            break
+        if tentativa in lista_letras_palavras:
+            print("Você acertou a letra!")
 
-    # Condicional fora do while:
-    if "_" in letras_descobertas:
-        print(f"\nVocê perdeu. A palavra era: {palavra}")
+            # Loop
+            for indice in range(len(lista_letras_palavras)):
+
+                # Condicional 3
+                if lista_letras_palavras[indice] == tentativa:
+                    tabuleiro[indice] = tentativa
+
+            # Se todos os espaços foram preenchidos, o jogo acabou
+            if "_" not in tabuleiro:
+                print(f"\nVocê venceu! A palavra era: {palavra}")
+                break
+
+        else:
+            print("Ops. Essa letra não está na palavra!")
+            chances -= 1
+
+    # Condicional fora do while
+    if "_" in tabuleiro:
+        print(f"\nVocê perdeu! A palavra era: {palavra}")
 
 
+# Principal
 game()
