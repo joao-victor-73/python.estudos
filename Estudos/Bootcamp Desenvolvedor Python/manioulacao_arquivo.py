a1aa42008f862a7aa7095c77c504ebde87c8e5f2
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2023-05-15 15:01:19 -0300

    Começando estudos do Bootcamp da XpE.
    Capítulo 8 sobre Manipulação de Arquivos (sempre útil prestar atenção sobre manipulação de arquvios)

---
+++ Estudos/Bootcamp Desenvolvedor Python/manioulacao_arquivo.py
@@ -0,0 +1,10 @@
+# Aula 8.1 - Criação, Abertura e Fechamento de Arquivos
+
+'''                     << DIFERENTES MODOS DE USO PARA UM ARQVIVO: >>
+'r' - Modo somente leitura (modo padrão);
+'w' - Modo de escrita. Cria um arquivo, caaso ainda não exista, ou substítui o arquivo atual;
+'x' - Modo de escrita. Cria um arquivo e, se o arquivo já existir, retorna um erro.
+'a' - Modo de escrita. Cria um arquivo, caso ainda não exista e adiciona dados ao final dele.
+'t' - Abre o arquivo no modo texto (modo padrão);
+'b' - Abre o arquivo no modo binário.
+'''
