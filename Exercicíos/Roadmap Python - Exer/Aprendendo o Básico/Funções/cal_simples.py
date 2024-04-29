acece8621c213abbba6b41d42d22f9c210624974
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-04-28 21:55:55 -0300

    add files

---
+++ Exercicíos/Roadmap Python - Exer/Aprendendo o Básico/Funções/cal_simples.py
@@ -0,0 +1,6 @@
+"""
+                                        Calculadora Simples: 
+Crie uma calculadora simples que permita ao usuário escolher entre operações de soma, 
+subtração, multiplicação e divisão. Cada operação deve ser implementada como uma função separada.
+"""
+
