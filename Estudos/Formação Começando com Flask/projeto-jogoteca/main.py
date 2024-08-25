ec8d1942158fe464a38cd544d3d0d2d3a60ad615
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-08-25 15:08:46 -0300

    adicionando o bootstrap na pasta static

--- Estudos/Formação Começando com Flask/projeto-jogoteca/main.py
+++ Estudos/Formação Começando com Flask/projeto-jogoteca/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
@@ -11,18 +11,35 @@ class Jogo:
         self.console = console
 
 
+jogo1 = Jogo("Mario Bros 3", "Plataforma", "SNES")
+jogo2 = Jogo("Contra 4", "Ação", "PS2")
+jogo3 = Jogo("Devil May Cry", "Rogue Like", "Ação")
+lista = [jogo1, jogo2, jogo3]
+
+
 @app.route('/')
 def inicio():
-    jogo1 = Jogo("Mario Bros 3", "Plataforma", "SNES")
-    jogo2 = Jogo("Contra 4", "Ação", "PS2")
-    jogo3 = Jogo("Devil May Cry", "Rogue Like", "Ação")
-    lista = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo='Jogos', lista_jogos=lista)
 
 
 @app.route('/novo')
 def novo_jogo():
     return render_template('novo.html', titulo='Novo Jogo')
+
+
+@app.route('/criar', methods=['POST', ])
+def criar_jogo():
+    nome_jogo = request.form.get('nome', False)
+    categoria_jogo = request.form.get('categoria', False)
+    console_jogo = request.form.get('console', False)
+
+    # Instânciando a classe Jogo com as informações do formulário do HTML
+    jogo = Jogo(nome_jogo, categoria_jogo, console_jogo)
+
+    # Adicionando as informações da classe Jogo na lista
+    lista.append(jogo)
+
+    return redirect('/')
 
 
 if __name__ == '__main__':
