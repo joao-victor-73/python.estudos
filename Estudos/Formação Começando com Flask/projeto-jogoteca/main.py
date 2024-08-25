f8ba2a44f7de505f1a1f2b4cc7f600735d12e8a0
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-08-25 13:23:55 -0300

    criação das duas primeiras páginas

--- Estudos/Formação Começando com Flask/projeto-jogoteca/main.py
+++ Estudos/Formação Começando com Flask/projeto-jogoteca/main.py
@@ -4,5 +4,26 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
+@app.route('/')
+def inicio():
+    jogo1 = Jogo("Mario Bros 3", "Plataforma", "SNES")
+    jogo2 = Jogo("Contra 4", "Ação", "PS2")
+    jogo3 = Jogo("Devil May Cry", "Rogue Like", "Ação")
+    lista = [jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo='Jogos', lista_jogos=lista)
+
+
+@app.route('/novo')
+def novo_jogo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
