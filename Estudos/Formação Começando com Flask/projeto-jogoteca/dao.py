4b6614ba502fb2d09862f9154fc4f6c4cf234e96
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-08-31 20:00:06 -0300

    Update dao.py

--- Estudos/Formação Começando com Flask/projeto-jogoteca/dao.py
+++ Estudos/Formação Começando com Flask/projeto-jogoteca/dao.py
@@ -1,4 +1,4 @@
-from .models improt Jogo, Usuario
+from .models import Jogo, Usuario
 
 SQL_DELETA_JOGO = "DELETE FROM jogo WHERE id = %s"
 SQL_JOGO_POR_ID = "SELECT id, nome, console FROM jogo WHERE id = %s"
@@ -12,6 +12,7 @@ class JogoDao:
     def __init__(self, db):
         self.__db = db
 
+
     def salvar(self, jogo):
         cursor = self.__db.connection.cursor()
 
@@ -23,18 +24,21 @@ class JogoDao:
         self.__db.connection.commit()
         return jogo
     
+
     def listar(self):
         cursor = self.__db.connection.cursor()
         cursor.execute(SQL_BUSCA_JOGOS)
         jogos = traduz_jogos(cursor.fetchall())
         return jogos
     
+
     def buscar_por_id(self, id):
         cursor = self.__db.connection.cursor()
         cursor.execute(SQL_JOGO_POR_ID, (id))
         tupla = cursor.fetchone()
         return Jogo(tupla[1], tupla[2], tupla[3], id=tupla[0])
     
+
     def deletar(self, id):
         self.__db.connection.cursor().execute(SQL_DELETA_JOGO, (id, ))
         self.__db.connection.commit()
@@ -43,6 +47,7 @@ class JogoDao:
 class UsuarioDao:
     def __init__(self, db):
         self.__db = db
+
 
     def buscar_por_id(self, id):
         cursor = self.__db.connection.cursor()
