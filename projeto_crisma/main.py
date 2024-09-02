3e3ec0d9f175e065e9c43a751675a2a332e79c5c
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-09-01 22:03:46 -0300

    preparando páginas para projeto crisma

--- projeto_crisma/main.py
+++ projeto_crisma/main.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return render_template('index.html', titulo='Jogos')
 
+
+@app.route('/crismandos')
+def crismandos():
+    return render_template('crismandos.html')
+
+
+@app.route('/chamada')
+def chamada():
+    return render_template('chamada.html')
 
 
 if __name__ == ('__main__'):
