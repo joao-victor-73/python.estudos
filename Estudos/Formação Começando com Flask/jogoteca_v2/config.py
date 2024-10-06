7ab21cfb169b6361db8500418d95e23c19500701
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-10-06 14:31:24 -0300

    Opção de colocar uma capa ao jogo

--- Estudos/Formação Começando com Flask/jogoteca_v2/config.py
+++ Estudos/Formação Começando com Flask/jogoteca_v2/config.py
@@ -1,4 +1,8 @@
+import os
+
 SECRET_KEY = 'alura'
 
 # Fazendo a conexão com o banco de dados criando uma configuração no app
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:darc147@localhost/jogoteca'
+
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
--- Estudos/Formação Começando com Flask/jogoteca_v2/views.py
+++ Estudos/Formação Começando com Flask/jogoteca_v2/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask import Flask, render_template, request, redirect, session, flash, url_for, send_from_directory
 from jogoteca import app, db
 from models import Jogos, Usuarios
 
@@ -42,6 +42,10 @@ def criar():
     novo_jogo = Jogos(nome=nome, categoria=categoria, console=console)
     db.session.add(novo_jogo)
     db.session.commit()
+
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOAD_PATH']
+    arquivo.save(f'{upload_path}/capa{novo_jogo.id}.jpg')
 
     return redirect(url_for('index'))
 
@@ -111,3 +115,8 @@ def logout():
     session['usuario_logado'] = None
     flash('Logout efetuado com sucesso!')
     return redirect(url_for('index'))
+
+
+@app.route('/uploads/<nome_arquivo>')
+def imagem(nome_arquivo):
+    return send_from_directory('uploads', nome_arquivo)
