09661560e8b3c33c73dc3cff89e64c39a80d5590
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2025-03-21 17:33:34 -0300

    praticando (3)

--- Estudos/Praticando APIs/Desenvolvendo API/api/__init__.py
+++ Estudos/Praticando APIs/Desenvolvendo API/api/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 api = Api(app)
 
--- Estudos/Praticando APIs/Desenvolvendo API/config.py
+++ Estudos/Praticando APIs/Desenvolvendo API/config.py
@@ -1,1 +1,10 @@
 DEBUG = True
+
+# Banco de dados
+USERNAME = 'root'
+PASSWORD = 'darc147'
+SERVER = 'localhost'
+DB = 'api_flask'
+
+SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
