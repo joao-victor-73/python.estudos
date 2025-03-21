a1d10e08fa03cb45accbbc4d5a2641c6cfc2813b
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2025-03-21 12:30:05 -0300

    praticando desenvolvimento API
    Instalando as bibliotecas e criando as pastas.
    Além de gerar um requeriments

---
+++ Estudos/Praticando APIs/praticando_json.py
@@ -0,0 +1,11 @@
+"""
+                                        O QUE É JSON?
+JSON é um formato baseado em chaves e valores, semelhante a um dicionário Python. Exemplo de um JSON:
+
+{
+  "nome": "João",
+  "idade": 25,
+  "cidade": "São Paulo",
+  "habilidades": ["Python", "Flask", "SQL"]
+}
+"""
