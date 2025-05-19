28e3a3c862ec95e42eb859ba000170db8e0c5e92
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2025-05-19 20:18:10 -0300

    desafio python (5)

---
+++ Exercicios/Desafios Python/06 - .py
@@ -0,0 +1,32 @@
+""""20/05/2025 - Desafio traz tuplas e listas combinadas para práticar
+
+Desafio 06: Alunos aprovados e reprovados
+    Crie um programa que receba uma lista de tuplas, onde cada tupla contém o nome de um aluno e sua nota final (de 0 a 10).
+    Você deve:
+        -> Separar os alunos aprovados (nota ≥ 6) dos reprovados (nota < 6).
+        -> Retornar duas listas:
+        -> Uma com os nomes dos aprovados.
+        -> Outra com os nomes dos reprovados.
+        -> Exibir também a média geral da turma.
+    
+    Exemplo de entrada:
+        alunos = [
+            ("Ana", 7.5),
+            ("Bruno", 5.0),
+            ("Carlos", 8.0),
+            ("Daniela", 4.5),
+            ("Eduardo", 6.0)
+        ]
+    
+
+    Saída Esperada:
+        Aprovados: ['Ana', 'Carlos', 'Eduardo']
+        Reprovados: ['Bruno', 'Daniela']
+        Média da turma: 6.2
+
+        
+    REGRAS:
+        -> Use estruturas como listas e tuplas.
+        -> Faça validações se quiser (ex: ignorar notas inválidas).
+        -> Se quiser, pode deixar os nomes em ordem alfabética.
+"""
