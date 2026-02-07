692ec3d80678107a388eb019e0260613b867a5d7
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2026-02-07 16:38:26 -0300

    att repository

--- Estudos/Módulos e Bibliotecas em Python/modulo_regex.py
+++
@@ -1,5 +0,0 @@
-import re
-
-text = "Udemy - uma plataforma com muitos cursos"
-
-# 1 - Indice inicial e final de palavras
--- Exercicios/Exercicios praticos e curtos/2-dicionario_lista.py
+++
@@ -1,7 +0,0 @@
-"""
-Receba nomes de alunos e suas notas e:
-
-    - Calcule a média
-    - Liste os alunos aprovados (média >= 7)
-    - Encontre quem tirou a maior e a menor nota
-"""
